b8de261ae0a2e84ad150989e831c7589f1928754
valid <valid.saley@gmail.com>
2020-08-06 21:04:03 +0300

    Building a recommendation engine

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -4,6 +4,7 @@ from .cart import Cart
 from .forms import CartAddProductForm
 from django.views.decorators.http import require_POST
 from coupon.forms import CouponApplyForm
+from shop.recommender import Recommender
 
 
 @require_POST
@@ -31,4 +32,12 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
                                                                    'override': True})
     coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products,
+                                                  max_results=4)
+
+    return render(request, 'cart/detail.html', {'cart': cart,
+                                                'coupon_apply_form': coupon_apply_form,
+                                                'recommended_products': recommended_products})
